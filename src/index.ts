49023bfb53acf278482fc77c033378cfba34cedf
Julien Lemaire <julien.lemaire@sticmac.fr>
2022-05-18 18:51:52 +0200

    feat: Socket.io integration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import path from "path";
+import { Server } from "socket.io";
+import http from "http";
+
 const app = express();
 const port = 8080; // default port to listen
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Configure Express to use EJS
 app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
+
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
@@ -13,8 +21,15 @@ app.get( "/", ( req, res ) => {
     res.render( "index" );
 } );
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 // start the express server
-app.listen( port, () => {
+server.listen( port, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
 } );
