9fca75d9ec4362a4ef2102c4502d565ccc5bb717
Julien Lemaire <julien.lemaire@sticmac.fr>
2022-05-26 15:48:19 +0200

    feat: Subpath and custom port support

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,33 @@ import path from "path";
 import { Server } from "socket.io";
 import http from "http";
 
+const subPath = process.env.SUB_PATH || '';
+
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 8080; // default port to listen
 const server = http.createServer(app);
+
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+app.set("views", path.join( __dirname, "views"));
+app.set("view engine", "ejs");
 
+const router = express.Router();
+
+router.use(express.static(path.join(__dirname, 'public')));
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+router.get( "/", ( req, res ) => {
     // render the index template
     res.render( "index" );
 } );
 
+app.use(`/${subPath}`, router);
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
